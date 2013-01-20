0e83f2488db95b46fc93b38949ff03e20ec28e9d
Mishal Awadah <a.mamish@gmail.com>
2013-01-20 15:00:23 -0500

    Stable scraper, needs complete implementation.

--- pandora_scraper.py
+++ pandora_scraper.py
@@ -1,18 +1,22 @@
 from selenium import webdriver
 # For keyboard keys
 from selenium.webdriver.common.keys import Keys
+import selenium.webdriver.support.ui as ui
 
 # Headless display
-from pyvirtualdisplay import Display
-display = Display(visible=0, size=(800, 600))
-display.start()
+# from pyvirtualdisplay import Display
+# display = Display(visible=0, size=(800, 600))
+# display.start()
 
 # Setup
 driver = webdriver.Firefox()
 driver.get("http://www.pandora.com")
+wait = ui.WebDriverWait(driver, 10)
 
 # Find and login
-signinLink = driver.find_element_by_xpath('//a[@class="signInLink underline"]')
+signinLink = driver.find_element_by_xpath("//a[@class='signInLink underline']")
+wait.until(lambda driver: driver.find_element_by_xpath("//a[@class='signInLink underline']").is_displayed())
+#wait.until(lambda x: siginInLink.isDisplayed())
 signinLink.click()
 
 
@@ -22,4 +26,4 @@ signinLink.click()
 # elem.send_keys(Keys.RETURN)
 
 driver.close()
-display.stop()
+#display.stop()
